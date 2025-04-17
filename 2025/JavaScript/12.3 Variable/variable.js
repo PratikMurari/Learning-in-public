cc580c7be2887178cf509f23f0d1925bafbb3786
Pratik Murari <pratikmurari007@gmail.com>
2025-04-18 01:26:26 +0530

    Done Variables

---
+++ 2025/JavaScript/12.3 Variable/variable.js
@@ -0,0 +1,3 @@
+var myName = "pratik";
+
+console.log("Hi " + myName);
---
+++ 2025/JavaScript/12.4 Variables Exercise/exercise.js
@@ -0,0 +1,16 @@
+function test() {
+  var a = "3";
+  var b = "8";
+
+  /***********Do not change the code above 👆*******/
+  //Write your code on lines 7 - 9:
+  var c = a;
+  a = b;
+  b = c;
+  /***********Do not change the code below 👇*******/
+
+  console.log("a is " + a);
+  console.log("b is " + b);
+}
+
+test(); // ← This runs the function
