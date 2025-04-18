c7364d3c2fb3adf5b7f47ed5e67cc0085689906b
Pratik Murari <pratikmurari007@gmail.com>
2025-04-19 04:04:48 +0530

    string length done

---
+++ 2025/JavaScript/12.7 String Lengths and Retrieving the Number of Characters/stringlength.js
@@ -0,0 +1,5 @@
+//var.length gives the length of the syring
+
+var twit="Hi I am Pratik Murari";
+
+console.log("you have written " + twit.length + " characters, and you have left " + (140-twit.length)+ " characters");
