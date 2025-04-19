e71ed83d17a6298db74ff952527a86c8e3ffb30a
Pratik Murari <pratikmurari007@gmail.com>
2025-04-20 00:36:57 +0530

    Basic Arithmetic Done

---
+++ 2025/JavaScript/12.10 Basic Arithmetic/arithmetic.js
@@ -0,0 +1,25 @@
+//Addition
+var a = 2 + 3; //5
+//Subtraction
+var b = 3 - 2; //1
+//Multiplication
+var c = 2 * 3; //6
+//Division
+var d = 3 / 2; //1.5
+//Modulus
+var e = 3 % 2; //1 (it is remainder of 3/2)
+//Exponention
+var f = 2 ** 3; //8 (it is 2 raised to the power of 3)
+//Increment
+var g = 5;
+g++;
+//Decrement
+g--;
+//there is also stuff like
+g += 2; //7
+g -= 2; //5
+g += a; //10
+
+//challenge calculate human age of your dog
+var dogAge = 4;
+console.log("Your dog's age in human years is: " + ((dogAge - 2) * 4 + 21));
