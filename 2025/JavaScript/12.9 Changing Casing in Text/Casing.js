8eb2bf13832eb6baf8ca50dbada9e6651b3094f3
Pratik Murari <pratikmurari007@gmail.com>
2025-04-19 22:00:56 +0530

    Changing Casing in Text

---
+++ 2025/JavaScript/12.9 Changing Casing in Text/Casing.js
@@ -0,0 +1,10 @@
+var name= "Pratik";
+
+//Changing the casing of a string to all uppercase
+console.log(name.toUpperCase());
+//Changing the casing of a string to all uppercase
+console.log(name.toLowerCase());
+
+//Changing the casing of a string to have only first character as uppercase
+console.log(name.slice(0,1).toUpperCase()+name.slice(1).toLowerCase())
+
