720831b443daaba81923bbc7e78d859c4dfd0c65
Pratik Murari <pratikmurari007@gmail.com>
2025-04-21 21:39:23 +0530

    Random Number Generator

---
+++ 2025/JavaScript intermediate/13.1  RNG.js
@@ -0,0 +1,15 @@
+//Random Number Generator
+var n=Math.random()*100;        //storing the random number in variable n
+console.log(n);
+n=Math.floor(n);
+console.log(n);
+
+//Math.random() generates a random 16 difgit number between 0 and 1
+//Math.floor(var) rounds the number down to the nearest whole number
+
+
+//create a Love Score Calculator
+console.log("Your Love Score is: " + (Math.floor(Math.random()*100)+1)+"%");
+
+//This will generate a random number between 1 and 100
+//could be used
