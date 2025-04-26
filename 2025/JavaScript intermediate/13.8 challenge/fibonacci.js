8a1ec9e90043dd1a22fe14d34e83f06db6195b9c
Pratik Murari <pratikmurari007@gmail.com>
2025-04-26 23:28:13 +0530

    fixing some bugs

--- 2025/JavaScript intermediate/13.8 challenge/fibonacci.js
+++ 2025/JavaScript intermediate/13.8 challenge/fibonacci.js
@@ -2,14 +2,13 @@ function fibonacciGenerator(n) {
   //Do NOT change any of the code above 👆
 
   //Write your code here:
-  var i = 0;
   var output = [];
   if (n === 1) {
     return [0];
   } else if (n === 2) {
     return [0, 1];
   } else {
-    var output = [0, 1];
+    output = [0, 1];
     for (var i = 2; i < n; i++) {
       output.push(output[i - 2] + output[i - 1]);
     }
@@ -20,3 +19,7 @@ function fibonacciGenerator(n) {
 
   //Do NOT change any of the code below 👇
 }
+
+console.log(fibonacciGenerator(1));
+console.log(fibonacciGenerator(2));
+console.log(fibonacciGenerator(15));
