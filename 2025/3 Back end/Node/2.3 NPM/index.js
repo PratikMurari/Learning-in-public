b082ebd1cfe29694aac969aa15e6a25e0978785f
Pratik Murari <pratikmurari007@gmail.com>
2025-06-26 21:35:16 +0530

    bugfix: fixed import issue with superheroes by using named export

--- 2025/3 Back end/Node/2.3 NPM/index.js
+++ 2025/3 Back end/Node/2.3 NPM/index.js
@@ -1,10 +1,11 @@
 // var generateName = require('sillyname');
 
 //or we can use import instead of require
-import generateName from 'sillyname';
-var sillyName = generateName();
 
-console.log ('Hello, my name is '+sillyName+'.');
+//code
+//import generateName from 'sillyname';
+//var sillyName = generateName();
+//console.log ('Hello, my name is '+sillyName+'.');
 
 //to install any package, use the command: // npm install <package-name> or npm i <package-name>
 //for multiple package, just use space to separate them eg: npm i <package-name> <package-name> <package-name>
@@ -13,7 +14,7 @@ console.log ('Hello, my name is '+sillyName+'.');
 //it kinda standardise the frontend and backend code, so we can use the same syntax in both places
 //to use import, we need to add "type": "module" in package.json file
 
-import superheroes from "superheroes";
-const name = superheroes.random();
-
-console.log(`My superhero name is ${name}.`);
+//code
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}.`);
