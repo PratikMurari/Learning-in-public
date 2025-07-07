6599186d674472d4b791846ba6c986d2723fff69
Pratik Murari <pratikmurari007@gmail.com>
2025-07-07 21:38:07 +0530

    done with js test 1

---
+++ 2025/Sigma/js/Chapter 1/test.js
@@ -0,0 +1,35 @@
+//Chapter 1 - Q1
+let a = "apple";
+let b = 1;
+console.log(a+b);
+
+
+//Chapter 1 - Q2
+console.log(typeof (a+b));
+
+
+//Chapter 1 - Q3
+const x ={
+    "name": "Pratik",
+    "last name": "Murari",
+}
+// x=66;
+console.log (x);
+//Nope it won't allow
+
+
+//Chapter 1 - Q4
+x["friend"] = "Adarsh";
+//x["name"] = "love";             you can change add delete things from object but you can't change to different data type
+console.log (x);
+
+
+//Chapter 1 - Q5
+const dict ={
+    Ephemeral : "lasting for a very short time.",
+    Serendipity: "the occurrence of events by chance in a happy or beneficial way.",
+    Petrichor: "the pleasant, earthy smell after rain.",
+    Liminal: "relating to a transitional or initial stage; on the threshold.",
+    Ineffable: "too great or extreme to be expressed in words.",
+}
+console.log(dict.Ineffable);
