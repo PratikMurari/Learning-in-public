f3f0d76c2dde2df35593d03f54f00ee76905cd5e
Pratik Murari <pratikmurari007@gmail.com>
2025-07-10 13:44:43 +0530

    js ch5q3

--- 2025/Sigma/js/test/ch5_test.js
+++ 2025/Sigma/js/test/ch5_test.js
@@ -5,13 +5,20 @@
 
 
 //Chapter 5 - Q2
-let num = [1, 2, 3, 4];
-let n;
-do{
-  n = prompt("add a number to arry: ");
-  num.push(n)
-  console.log(num);
-}
-while (n = 0) {
-    console.log(num);
-}
+// let num = [1, 2, 3, 4];
+// let n;
+// do{
+//   n = prompt("add a number to arry: ");
+//   num.push(n)
+//   console.log(num);
+// }
+// while (n = 0) {
+//     console.log(num);
+// }
+
+//Chapter 5 - Q3
+let arr = [5,10,15,16,20,40];
+let n = arr.filter((x)=>{
+  return x % 10 == 0;
+});
+console.log(n)
