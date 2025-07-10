66ad9813747c448c351154549917075482ff57a9
Pratik Murari <pratikmurari007@gmail.com>
2025-07-10 13:16:09 +0530

    js ch5q2 done

--- 2025/Sigma/js/test/ch5_test.js
+++ 2025/Sigma/js/test/ch5_test.js
@@ -1,5 +1,17 @@
 //Chapter 5 - Q1
-let num = [1,2,3,4];
-n= prompt("add a number to arry: ");
-console.log(num.push(n));
+// let num = [1,2,3,4];
+// n= prompt("add a number to arry: ");
+// console.log(num.push(n));
 
+
+//Chapter 5 - Q2
+let num = [1, 2, 3, 4];
+let n;
+do{
+  n = prompt("add a number to arry: ");
+  num.push(n)
+  console.log(num);
+}
+while (n = 0) {
+    console.log(num);
+}
