1af9c5e038e813c98bf3d4eaf6e446019fff49a9
Pratik Murari <pratikmurari007@gmail.com>
2025-07-26 21:20:14 +0530

    completed

--- 2025/3 Back end/React/6 Hooks/2 React Hooks - useState/src/components/App.jsx
+++ 2025/3 Back end/React/6 Hooks/2 React Hooks - useState/src/components/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  let [count, setCount] = useState(0);
+
+  function increse() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increse}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- 2025/3 Back end/React/6 Hooks/2 React Hooks - useState/src/index.js
+++ 2025/3 Back end/React/6 Hooks/2 React Hooks - useState/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 import "./../public/styles.css";
 
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
