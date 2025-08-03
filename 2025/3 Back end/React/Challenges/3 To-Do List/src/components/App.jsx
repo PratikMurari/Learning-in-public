776dbdb2122d6a391d8a22d155483e7ae5341380
Pratik Murari <pratikmurari007@gmail.com>
2025-08-03 22:21:37 +0530

    To-Do List Challenge 2 & 3 completed

--- 2025/3 Back end/React/Challenges/3 To-Do List/src/components/App.jsx
+++ 2025/3 Back end/React/Challenges/3 To-Do List/src/components/App.jsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     const newValue = event.target.value;
     setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
   }
 
   return (
@@ -15,13 +23,15 @@ function App() {
       </div>
       <div className="form">
         <input onChange={handleChange} type="text" value={inputText} />
-        <button>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map(todoItem => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
