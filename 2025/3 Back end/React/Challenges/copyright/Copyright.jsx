2135d1006adbd6446c31f5f09ce1a89104f08599
Pratik Murari <pratikmurari007@gmail.com>
2025-08-08 01:50:34 +0530

    Copyright

--- 2025/3 Back end/React/Challenges/copyright/Copyright.jsx
+++ 2025/3 Back end/React/Challenges/copyright/Copyright.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 function Copyright() {
   const year = new Date().getFullYear();
-  return (
-    <p style={{ textAlign: "center", fontSize: "1 em", color: "#5A584E" }}>
-      &copy; {year} All rights reserved by FurtherAI Inc.
-    </p>
-  );
+  return <p>&copy; {year} by Company. All rights reserved.</p>;
 }
 
 export default Copyright;
