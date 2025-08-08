4482fa865d8d5790f84bb5e75ac4207ddf7be609
Pratik Murari <pratikmurari007@gmail.com>
2025-08-09 00:56:46 +0530

    Implement code changes to enhance functionality and improve performance

--- 2025/3 Back end/React/Challenges/copyright/Copyright.jsx
+++ 2025/3 Back end/React/Challenges/copyright/Copyright.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Copyright() {
   const year = new Date().getFullYear();
-  return <p>&copy; {year} by Company. All rights reserved.</p>;
+  return <p>&copy; {year} by Certus AI. All rights reserved.</p>;
 }
 
 export default Copyright;
