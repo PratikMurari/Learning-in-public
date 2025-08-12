4a5ed698a1cd15092bca04da9d55c96fa3edd8ca
Pratik Murari <pratikmurari007@gmail.com>
2025-08-12 23:27:01 +0530

    Chapter 6 - Q2 done

--- 2025/Sigma/js/test/ch6_test.js
+++ 2025/Sigma/js/test/ch6_test.js
@@ -12,3 +12,23 @@ if (age !== null) {  // Check if the user didn't click Cancel
     alert("You cannot drive yet.");
   }
 }
+
+//Chapter 6 - Q2
+// Ask if the user wants to see the prompt
+if (confirm("Do you want to enter your age to check if you can drive?")) {
+  let age = prompt("Please enter your age:");
+
+  if (age !== null) {  // Check if the user didn't click Cancel
+    age = Number(age);
+
+    if (isNaN(age)) {
+      alert("Please enter a valid number for age.");
+    } else if (age >= 18) {
+      alert("You can drive!");
+    } else {
+      alert("You cannot drive yet.");
+    }
+  }
+} else {
+  alert("Alright! Maybe next time.");
+}
