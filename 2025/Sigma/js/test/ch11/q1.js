4d5e58567eeea2ead5e172360123d57699db534b
Pratik Murari <pratikmurari007@gmail.com>
2025-09-07 19:45:03 +0530

    ch 11 q1 done

---
+++ 2025/Sigma/js/test/ch11/q1.js
@@ -0,0 +1,12 @@
+// Class to represent a complex number
+class ComplexNumber {
+  /**
+   * Constructor to initialize the complex number
+   * @param {number} real - The real part of the complex number
+   * @param {number} imaginary - The imaginary part of the complex number
+   */
+  constructor(real, imaginary) {
+    this.real = real; // Set the real part
+    this.imaginary = imaginary; // Set the imaginary part
+  }
+}
