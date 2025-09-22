84b36c9f438ab69d246b9c718b68169a0c055dbd
Pratik Murari <pratikmurari007@gmail.com>
2025-09-22 13:59:42 +0530

    Section 5: Number, Boolean, and type inference & Added a new section for code play aroud

---
+++ 2025/3 Back end/TypeScript/Code/Section 3/intro.js
@@ -0,0 +1,5 @@
+var user = { name: "Pratik", age: 25 };
+console.log("Pratik");
+console.log(user.name);
+console.log(user.age);
+//to run typescript file use command tsc filename.ts
---
+++ 2025/3 Back end/TypeScript/Code/Section 3/intro.ts
@@ -0,0 +1,7 @@
+var user = {name: "Pratik", age: 25}
+
+console.log("Pratik");
+console.log(user.name);
+console.log(user.age);
+
+//to run typescript file use command tsc filename.ts
