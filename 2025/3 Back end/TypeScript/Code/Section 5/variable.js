3040c707c99eead950b84f1f73b68e1ab4b8985c
Pratik Murari <pratikmurari007@gmail.com>
2025-09-23 13:38:25 +0530

    section 5 done done, "Number, Boolean and type of inference"

--- 2025/3 Back end/TypeScript/Code/Section 5/variable.js
+++ 2025/3 Back end/TypeScript/Code/Section 5/variable.js
@@ -4,9 +4,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var userName = "Pratik";
 var greetings = "Hello, ".concat(userName);
-// greetings= true
-var userAge = 25;
-//userAge.toUpperCase();
 greetings.toLowerCase();
 //it helps you to avoid mistakes
 console.log(greetings);
+// number
+var userId = 2233346.7;
+var newUserId = userId.toFixed();
+//userId= "Pratik";
+console.log(newUserId);
+// boolean
+var isLoggedIn = false;
+console.log(isLoggedIn);
--- 2025/3 Back end/TypeScript/Code/Section 5/variable.ts
+++ 2025/3 Back end/TypeScript/Code/Section 5/variable.ts
@@ -4,15 +4,23 @@
 let userName: string = "Pratik";    
 let greetings: string = `Hello, ${userName}`; 
 
-// greetings= true
-
-let userAge: number =25; 
-//userAge.toUpperCase();
-
 greetings.toLowerCase();
 //it helps you to avoid mistakes
-
 console.log(greetings);
+
+
+// number
+let userId = 2233346.7;
+
+let newUserId= userId.toFixed();
+//userId= "Pratik";
+
+console.log(newUserId);
+
+// boolean
+let isLoggedIn: boolean = false; 
+
+console.log(isLoggedIn);
 
 //adding this for now to make sure we can use let
 export{};
