5d9e8e3ef0e6cbc882cdc5e6f75547572d36e3ea
Pratik Murari <pratikmurari007@gmail.com>
2025-09-24 20:24:19 +0530

    Js code for section 8

--- 2025/3 Back end/TypeScript/Code/Section 8/myfunction.js
+++ 2025/3 Back end/TypeScript/Code/Section 8/myfunction.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //Function 1
+//This is how we defin a function type in tyscript
 function addTwo(num) {
     return num + 2;
+    //return "hello" //This will give an error as we have defined the return type as number
 }
 console.log(addTwo(7));
 //Function 2
@@ -17,3 +19,36 @@ var loginUser = function (name, email, isPaid) {
 };
 signUpUser("Pratik", "pratik@gmail.com", false);
 loginUser("P", "P@P.com"); //Normally it won't work, only if u provide a default value to it
+// **** New **** //
+// Function 4 (what we will do when we don't know the return type of the function)
+// function getValue(myVal:number):boolean{
+//     if(myVal > 5){
+//         return true;
+//     }
+//     return 100 ok;
+// }
+//Function 5, how to apply function type on Fat Arrow function
+var getHello = function (s) {
+    return "";
+};
+//Function 6
+var heros = ["batman", "thor", "ironman"];
+//const heros = [1, 2, 3];
+//Beauty about typescript is it can infer the type on its own
+// heros.map((hero) => {
+//   return `hero is ${hero}`;
+// });
+//But if we want to define the return type of the function then we can do it like this
+//And this is considered a better syntax
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+//Function 7 
+function consoleError(errmsg) {
+    console.log(errmsg);
+    //return 1; //This will give an error as we have defined the return type as void
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+    //return 1; //This will give an error as we have defined the return type as void
+}
