f82aa7b0ed96508c98771c522370aa1f3d7f4ab9
Pratik Murari <pratikmurari007@gmail.com>
2025-10-12 21:26:56 +0530

    trying to understand  type Aliases, will be back after a break

---
+++ 2025/stuDYING/TypeScript/Code/Section 10/myObjects.ts
@@ -0,0 +1,30 @@
+// const userName = {
+//   name: "Pratik",
+//   email: "coolPratik@gmail.com",
+//   isActive: true,
+// };
+
+// function createUser({ name, isPaid }: { name: string; isPaid: boolean }) {
+//   console.log(name);
+//   console.log(isPaid);
+// }
+
+// createUser({ name: "Pratik", isPaid: false });
+
+// function createCourse(): { name: string; price: number } {
+//   return { name: "ReactJS", price: 399 };
+// }
+
+type User = {
+  name: string;
+  email: string;
+  active: boolean;
+}
+
+function createUser(user: User): User {
+    return {name: "", email: "", active: true}
+}
+
+createUser({name:"", email:"", active: true})
+
+export {};
