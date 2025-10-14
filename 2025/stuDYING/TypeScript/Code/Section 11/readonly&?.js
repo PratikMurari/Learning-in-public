9041bf3a9a1e496ce1b9f88b4c171d0158a92153
Pratik Murari <pratikmurari007@gmail.com>
2025-10-14 22:11:14 +0530

    Add initial implementation of user object with utility types example

---
+++ 2025/stuDYING/TypeScript/Code/Section 11/readonly&?.js
@@ -0,0 +1,9 @@
+// Utility Types Source
+//https://www.typescriptlang.org/docs/handbook/utility-types.html
+var myUser = {
+    _id: "12344",
+    name: "Pratik",
+    email: "P@P.com",
+    isActive: false,
+};
+myUser.email = "p@mail";
