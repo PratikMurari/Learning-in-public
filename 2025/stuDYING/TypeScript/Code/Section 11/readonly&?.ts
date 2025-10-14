2df921e063c155240541cd31485b77741c5ea170
Pratik Murari <pratikmurari007@gmail.com>
2025-10-14 21:54:13 +0530

    Add TypeScript example demonstrating "readonly" and "?" properties in a User type

---
+++ 2025/stuDYING/TypeScript/Code/Section 11/readonly&?.ts
@@ -0,0 +1,19 @@
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  creditCardDetails?: number;
+};
+
+let myUser: User = {
+  _id: "12344",
+  name: "Pratik",
+  email: "P@P.com",
+  isActive: false,
+};
+
+myUser.email = "p@mail"
+
+// myUser._id = "3434" 
+// cannot reassign because of readonly
