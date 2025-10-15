1d13a3bc09d3ac44e5f412d3f5106574d455e180
Pratik Murari <pratikmurari007@gmail.com>
2025-10-15 22:51:04 +0530

    made arry in ts

---
+++ 2025/stuDYING/TypeScript/Code/Section 12/MyArray.ts
@@ -0,0 +1,17 @@
+const superHeros: string[] = [];
+//const heroPower: number[] = []
+//Another way of writing array
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "Bruce", isActive: true });
