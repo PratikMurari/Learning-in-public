16b26751672d02b048e0c3344e4d6381a1130591
Pratik Murari <pratikmurari007@gmail.com>
2025-10-16 11:48:35 +0530

    Add initial implementation of MyArray.js with example arrays and user objects

---
+++ 2025/stuDYING/TypeScript/Code/Section 12/MyArray.js
@@ -0,0 +1,9 @@
+var superHeros = [];
+//const heroPower: number[] = []
+//Another way of writing array
+var heroPower = [];
+var allUsers = [];
+var MLModels = [[255, 255, 255], []];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "Bruce", isActive: true });
