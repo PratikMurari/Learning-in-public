c65133a193f85b157331b9f4a4992d0a380fb113
Pratik Murari <pratikmurari007@gmail.com>
2025-10-21 20:00:20 +0530

    made the file lite weight

--- 2025/stuDYING/TypeScript/Code/Section 18/dist/index.js
+++ 2025/stuDYING/TypeScript/Code/Section 18/dist/index.js
@@ -1,3 +1,5 @@
 "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
 console.log("TypeScript is here!");
+console.log("TypeScript Amazing!");
 //# sourceMappingURL=index.js.map
--- 2025/stuDYING/TypeScript/Code/Section 18/src/index.ts
+++ 2025/stuDYING/TypeScript/Code/Section 18/src/index.ts
@@ -1,4 +1,5 @@
 console.log("TypeScript is here!");
+console.log("TypeScript Amazing!");
 
 export {};
 
