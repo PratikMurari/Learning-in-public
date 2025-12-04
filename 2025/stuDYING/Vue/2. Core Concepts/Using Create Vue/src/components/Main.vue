5d23d6a85987516791546a369b71b79e0c1f50e6
Pratik Murari <pratikmurari007@gmail.com>
2025-12-04 22:06:36 +0530

    Layout components in vue

---
+++ 2025/stuDYING/Vue/2. Core Concepts/Using Create Vue/src/components/Main.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+const name = ref("Pratik");
+</script>
+
+<template>
+  <main>
+    <h1>Hello, {{ name }}!</h1>
+    <p>
+      I'm about to learn
+      <a href="https://vuejs.org/" target="_blank">Vue.js</a>!
+    </p>
+  </main>
+</template>
+
+<style scoped>
+main h1 {
+  display: block;
+}
+main a {
+  color: #42b883;
+}
+</style>
