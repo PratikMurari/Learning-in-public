7890fe370d2f0a9b498c098a408240c15a27cb54
Pratik Murari <pratikmurari007@gmail.com>
2026-02-18 19:52:58 +0530

    Add initial implementation of union and any types in TypeScript with accompanying source maps

--- 2026/TypeScript/code/dist/3.union&any.js
+++ 2026/TypeScript/code/dist/3.union&any.js
@@ -1,29 +1,23 @@
-// union types allow us to specify that a variable can hold more than one type of value.
+"use strict";
+// union types allow us to specify that a variable can hold more than one type of value. 
 // We can use the pipe (|) operator to define a union type.
-
-let subs: number | string = "1M";
-
-let apiRequestStatus: "success" | "error" | "pending" = "pending";
-
+Object.defineProperty(exports, "__esModule", { value: true });
+let subs = "1M";
+let apiRequestStatus = "pending";
 apiRequestStatus = "done"; // Error: Type '"done"' is not assignable to type '"success" | "error" | "pending"'.
-
-let airlineSeat: "window" | "aisle" | "middle" = "window";
-
+let airlineSeat = "window";
 const orders = ["12", "20", "28", "42"];
-
-let currentOrder: string | undefined;
-
+let currentOrder;
 for (let order of orders) {
-  if (order === "28") {
-    currentOrder = order;
-    break;
-  }
-  currentOrder = "11";
+    if (order === "28") {
+        currentOrder = order;
+        break;
+    }
+    currentOrder = "11";
 }
-
 console.log(currentOrder);
-
 // any means usko parvah nahi hai ki usme kya value aayegi, wo kisi bhi type ki ho sakti hai.
 // try to avoid any as much as possible, because it defeats the purpose of using TypeScript,
 // which is to have type safety and catch errors at compile time.
 // Using any can lead to runtime errors that could have been caught during development.
+//# sourceMappingURL=3.union&any.js.map
