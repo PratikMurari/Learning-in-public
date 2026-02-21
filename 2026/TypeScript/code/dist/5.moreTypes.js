e53691f2432c6a5b65ec34d0bce464e6290b67e0
Pratik Murari <pratikmurari007@gmail.com>
2026-02-21 12:05:11 +0530

    Add type assertion example and JSON parsing for Book type

--- 2026/TypeScript/code/dist/5.moreTypes.js
+++ 2026/TypeScript/code/dist/5.moreTypes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let response = "42";
 // forceful type assertion, we are telling the compiler that we know better than it does about the type of response.
 let numericLength = response.length;
-// (response as string) is a type assertion that tells the compiler to treat response as a string, allowing us to access the length property without any type errors.
-// However, this can lead to runtime errors if response is not actually a string, so it should be used with caution.
+let bookString = '{"name": "who moved my cheese"}';
+let bookObject = JSON.parse(bookString);
+console.log(bookObject);
 //# sourceMappingURL=5.moreTypes.js.map
--- 2026/TypeScript/code/src/5.moreTypes.ts
+++ 2026/TypeScript/code/src/5.moreTypes.ts
@@ -4,3 +4,12 @@ let numericLength: number = (response as string).length;
 // (response as string) is a type assertion that tells the compiler to treat response as a string, allowing us to access the length property without any type errors.
 // However, this can lead to runtime errors if response is not actually a string, so it should be used with caution.
 
+// Basic example of type assertion in TypeScript
+type Book = {
+  name: string;
+};
+
+let bookString = '{"name": "who moved my cheese"}';
+let bookObject = JSON.parse(bookString) as Book;
+
+console.log(bookObject);
