ecee507fcf39862bf21acb5e292e3925f10d2032
Pratik Murari <pratikmurari007@gmail.com>
2026-02-23 13:43:01 +0530

    Add ChaiOrder interface and implement makeChai and serveChai functions for better code organization

---
+++ 2026/TypeScript/code/dist/8.interfaceTs.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=8.interfaceTs.d.ts.map
--- 2026/TypeScript/code/dist/8.interfaceTs.js
+++ 2026/TypeScript/code/dist/8.interfaceTs.js
@@ -1,17 +1,12 @@
+"use strict";
 // since we had common parameters in both makeChai and serveChai,
 // we can create an interface for it called ChaiOrder and use it as the type for the parameter of both functions.
 // which made our code more readable, reusable and easier to maintain.
-// 1 
-type ChaiOrder = {
-  type: string;
-  sugar: number;
-  strong: boolean;
-};
-
-function makeChai(order: ChaiOrder) {
-  console.log(order);
+Object.defineProperty(exports, "__esModule", { value: true });
+function makeChai(order) {
+    console.log(order);
 }
-
-function serveChai(order: ChaiOrder) {
-  console.log(order);
+function serveChai(order) {
+    console.log(order);
 }
+//# sourceMappingURL=8.interfaceTs.js.map
