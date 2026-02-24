64b5bc8bc51c29f0cfba402132d265d0bb93b775
Pratik Murari <pratikmurari007@gmail.com>
2026-02-24 09:53:01 +0530

    define interfaces in ts

--- 2026/TypeScript/code/dist/8.interfaceTs.js
+++ 2026/TypeScript/code/dist/8.interfaceTs.js
@@ -1,4 +1,5 @@
 "use strict";
+// 1
 // since we had common parameters in both makeChai and serveChai,
 // we can create an interface for it called ChaiOrder and use it as the type for the parameter of both functions.
 // which made our code more readable, reusable and easier to maintain.
@@ -8,5 +9,12 @@ function makeChai(order) {
 }
 function serveChai(order) {
     console.log(order);
+}
+class MasalaChai {
+    water = 100;
+    milk = 100;
+}
+class Chai {
+    size = "large";
 }
 //# sourceMappingURL=8.interfaceTs.js.map
--- 2026/TypeScript/code/src/8.interfaceTs.ts
+++ 2026/TypeScript/code/src/8.interfaceTs.ts
@@ -1,7 +1,8 @@
+// 1
 // since we had common parameters in both makeChai and serveChai,
 // we can create an interface for it called ChaiOrder and use it as the type for the parameter of both functions.
 // which made our code more readable, reusable and easier to maintain.
-// 1 
+
 type ChaiOrder = {
   type: string;
   sugar: number;
@@ -14,4 +15,25 @@ function makeChai(order: ChaiOrder) {
 
 function serveChai(order: ChaiOrder) {
   console.log(order);
+}
+
+// 2
+// we can also use interfaces to define the structure of objects that we want to create.
+// for example, we can create an interface for a tea recipe and then implement it in a class.
+type TeaRecipe = {
+  water: number;
+  milk: number;
+};
+
+class MasalaChai implements TeaRecipe {
+  water = 100;
+  milk = 100;
+}
+
+interface CupSize {
+  size: "small" | "large";
+}
+
+class Chai implements CupSize {
+  size: "small" | "large" = "large";
 }
