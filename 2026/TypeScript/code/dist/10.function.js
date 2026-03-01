15c57d1b319c0439306250310227463e676b2145
Pratik Murari <pratikmurari007@gmail.com>
2026-03-01 17:20:48 +0530

    Add initial implementation of function with TypeScript

---
+++ 2026/TypeScript/code/dist/10.function.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function makeChai(type, cups) {
+    console.log(`Making ${cups} cups of ${type}...`);
+}
+makeChai("Masala Chai", 2);
+//# sourceMappingURL=10.function.js.map
