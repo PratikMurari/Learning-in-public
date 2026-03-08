0a8b9c857af6383c90681db2d269373b08bcb0d5
Pratik Murari <pratikmurari007@gmail.com>
2026-03-08 21:57:14 +0530

    Protected keyword of tsc

--- 2026/TypeScript/code/dist/12.oop.js
+++ 2026/TypeScript/code/dist/12.oop.js
@@ -17,12 +17,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // masalaChai.flavour = "masala";
 // axis modifier
 class Chai {
+    // Public means it can be accessed from anywhere
     flavour = "Masala";
+    // Private means it can only be accessed within the class
     secretIngredient = "Cardamom";
     reveal() {
         return this.secretIngredient; //ok
         //this can't be direct accessed outside the class, but we can access it through a method inside the class
     }
 }
+class Shop {
+    // protected means it can be accessed within the class and its subclasses
+    shopName = "Chai corner";
+}
+// Branch is a subclass of Shop, so it can access the protected member shopName
+class Branch extends Shop {
+    getName() {
+        return this.shopName; //ok, because it is protected and we are accessing it within a subclass
+    }
+}
+// how to access protected members of a class
+new Branch().getName(); // now you can see you can access the shopName through the getName method of the Branch class, even though it is protected in the Shop class
+// how to access private members of a class
 const c = new Chai();
+c.reveal(); // ok, we can access the reveal method which returns the secret ingredient, but we can't access the secret ingredient directly because it is private
 //# sourceMappingURL=12.oop.js.map
--- 2026/TypeScript/code/src/12.oop.ts
+++ 2026/TypeScript/code/src/12.oop.ts
@@ -19,8 +19,10 @@
 
 // axis modifier
 class Chai {
+  // Public means it can be accessed from anywhere
   public flavour: string = "Masala";
 
+  // Private means it can only be accessed within the class
   private secretIngredient = "Cardamom";
 
   reveal() {
@@ -29,4 +31,20 @@ class Chai {
   }
 }
 
+class Shop {
+  // protected means it can be accessed within the class and its subclasses
+  protected shopName = "Chai corner";
+}
+
+// Branch is a subclass of Shop, so it can access the protected member shopName
+class Branch extends Shop {
+  getName() {
+    return this.shopName; //ok, because it is protected and we are accessing it within a subclass
+  }
+}
+// how to access protected members of a class
+new Branch().getName(); // now you can see you can access the shopName through the getName method of the Branch class, even though it is protected in the Shop class
+
+// how to access private members of a class
 const c = new Chai();
+c.reveal(); // ok, we can access the reveal method which returns the secret ingredient, but we can't access the secret ingredient directly because it is private
