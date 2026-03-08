7ffcedb467d17fa02959e07c9eef29897bd5a769
Pratik Murari <pratikmurari007@gmail.com>
2026-03-08 21:22:59 +0530

    Added axis modifer, and keywords like Public & Private in tsc

--- 2026/TypeScript/code/dist/12.oop.js
+++ 2026/TypeScript/code/dist/12.oop.js
@@ -1,17 +1,28 @@
 "use strict";
+// class Chai {
+//   flavour: string;
+//   //  price: number;
 Object.defineProperty(exports, "__esModule", { value: true });
+//   //   constructor(flavour: string, price: number) {
+//   //     this.flavour = flavour;
+//   //     this.price = price;
+//   //   }
+//   constructor(flavour: string) {
+//     this.flavour = flavour;
+//     console.log(this);
+//   }
+//   // this is just a value provided to the class, it is not a parameter of the constructor, so we can set it after creating an instance of the class
+// }
+// const masalaChai = new Chai("Ginger");
+// masalaChai.flavour = "masala";
+// axis modifier
 class Chai {
-    flavour;
-    //  price: number;
-    //   constructor(flavour: string, price: number) {
-    //     this.flavour = flavour;
-    //     this.price = price;
-    //   }
-    constructor(flavour) {
-        this.flavour = flavour;
-        console.log(this);
+    flavour = "Masala";
+    secretIngredient = "Cardamom";
+    reveal() {
+        return this.secretIngredient; //ok
+        //this can't be direct accessed outside the class, but we can access it through a method inside the class
     }
 }
-const masalaChai = new Chai("Ginger");
-masalaChai.flavour = "masala";
+const c = new Chai();
 //# sourceMappingURL=12.oop.js.map
--- 2026/TypeScript/code/src/12.oop.ts
+++ 2026/TypeScript/code/src/12.oop.ts
@@ -1,18 +1,32 @@
+// class Chai {
+//   flavour: string;
+//   //  price: number;
+
+//   //   constructor(flavour: string, price: number) {
+//   //     this.flavour = flavour;
+//   //     this.price = price;
+//   //   }
+
+//   constructor(flavour: string) {
+//     this.flavour = flavour;
+//     console.log(this);
+//   }
+//   // this is just a value provided to the class, it is not a parameter of the constructor, so we can set it after creating an instance of the class
+// }
+
+// const masalaChai = new Chai("Ginger");
+// masalaChai.flavour = "masala";
+
+// axis modifier
 class Chai {
-  flavour: string;
-  //  price: number;
+  public flavour: string = "Masala";
 
-  //   constructor(flavour: string, price: number) {
-  //     this.flavour = flavour;
-  //     this.price = price;
-  //   }
+  private secretIngredient = "Cardamom";
 
-  constructor(flavour: string) {
-    this.flavour = flavour;
-    console.log(this);
+  reveal() {
+    return this.secretIngredient; //ok
+    //this can't be direct accessed outside the class, but we can access it through a method inside the class
   }
-  // this is just a value provided to the class, it is not a parameter of the constructor, so we can set it after creating an instance of the class
 }
 
-const masalaChai = new Chai("Ginger");
-masalaChai.flavour = "masala";
+const c = new Chai();
