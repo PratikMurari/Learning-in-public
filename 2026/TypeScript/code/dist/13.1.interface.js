68eb5ce4979b249a3ff57cf5637b309bb05f257c
Pratik Murari <pratikmurari007@gmail.com>
2026-03-13 21:42:03 +0530

    add interfaces and generics files in TypeScript

---
+++ 2026/TypeScript/code/dist/13.1.interface.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// usage of interface
+const masala = {
+    flavor: "masala",
+    price: 10,
+};
+//usage of readonly properties in interface
+const s = {
+    id: 1,
+    name: "Chai corner",
+};
+// usage of function type interface
+const apply50 = (p) => p * 0.5; // 50% discount
+// usage of interface for class
+const machine = {
+    start() {
+        console.log("Tea machine started");
+    },
+    stop() {
+        console.log("Tea machine stopped");
+    },
+};
+//usage of index signature in interface
+const ratings = {
+    masala: 4.5,
+    ginger: 4.0,
+    // lemon: "3.5", // error, because the value must be a number
+};
+const u = {
+    name: "Pratik",
+    age: 24,
+};
+//# sourceMappingURL=13.1.interface.js.map
---
+++ 2026/TypeScript/code/dist/13.2.generics.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=13.2.generics.d.ts.map
