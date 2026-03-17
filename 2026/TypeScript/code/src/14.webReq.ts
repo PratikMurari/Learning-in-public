3658b4f495497c2f98eb0d3c381f6a0339bf36d4
Pratik Murari <pratikmurari007@gmail.com>
2026-03-17 22:14:38 +0530

    Standard Get Request

--- 2026/TypeScript/code/src/14.webReq.ts
+++ 2026/TypeScript/code/src/14.webReq.ts
@@ -1,1 +1,6 @@
 import axios from "axios";
+
+// Make a GET request to the specified URL and log the response data
+axios.get("https://example.com/data").then((response) => {
+  console.log(response.data);
+});
