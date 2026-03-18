462f07bc12df554722ae6662b0e63c28015dc7af
Pratik Murari <pratikmurari007@gmail.com>
2026-03-18 21:18:10 +0530

    feat: add Todo interface for response data structure in web request

--- 2026/TypeScript/code/src/14.webReq.ts
+++ 2026/TypeScript/code/src/14.webReq.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+// Define an interface for the expected structure of the response data
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 // Make a GET request to the specified URL and log the response data
 axios.get("https://example.com/data").then((response) => {
   console.log(response.data);
