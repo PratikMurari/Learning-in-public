cd3f2c00a779e448334d7e046504d83091ca4f19
Pratik Murari <pratikmurari007@gmail.com>
2026-03-20 19:10:32 +0530

    feat: implement fetchData function to retrieve Todo items from API

--- 2026/TypeScript/code/dist/14.2.fetchReq.js
+++ 2026/TypeScript/code/dist/14.2.fetchReq.js
@@ -1,2 +1,11 @@
+const fetchData = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        if (!response.ok) {
+            throw new Error(`HTTP error ${response.status}`);
+        }
+    }
+    catch (error) { }
+};
 export {};
 //# sourceMappingURL=14.2.fetchReq.js.map
--- 2026/TypeScript/code/src/14.2.fetchReq.ts
+++ 2026/TypeScript/code/src/14.2.fetchReq.ts
@@ -4,3 +4,12 @@ interface Todo {
   title: string;
   completed: boolean;
 }
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+  } catch (error: any) {}
+};
