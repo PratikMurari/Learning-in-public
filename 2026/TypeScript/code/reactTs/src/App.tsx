cc9c32234528cce2e84a00c5df81f922fb73955d
Pratik Murari <pratikmurari007@gmail.com>
2026-03-24 17:42:03 +0530

    feat: integrate ChaiList component and define menu data in App

--- 2026/TypeScript/code/reactTs/src/App.tsx
+++ 2026/TypeScript/code/reactTs/src/App.tsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import { ChaiCard } from "./components/ChaiCard";
 import { Counter } from "./components/Counter";
+import { ChaiList } from "./components/ChaiList";
+
+import type { Chai } from "./types";
 
 import "./App.css";
+
+const menu: Chai[] = [
+  { id: 1, name: "Green Tea", price: 299 },
+  { id: 2, name: "Masala Tea", price: 349 },
+  { id: 3, name: "Lemon Tea", price: 199 },
+];
 
 function App() {
   return (
     <>
+      // ChaiCard
       <div>
         <h1>Welcome to React (TypeScript Edition)</h1>
         <ChaiCard name="Green Tea" price={299} />
         <ChaiCard name="Masala Tea" price={349} isSpecial={true} />
       </div>
+      // Counter
       <div>
         <Counter />
+      </div>
+      // ChaiList
+      <div>
+        <ChaiList items={menu} />
       </div>
     </>
   );
