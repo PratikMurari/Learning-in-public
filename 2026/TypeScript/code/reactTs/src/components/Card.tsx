8093e0d8f416a285ca30c46442fc199eba6beb9c
Pratik Murari <pratikmurari007@gmail.com>
2026-03-25 19:45:41 +0530

    feat: add Card component for reusable UI structure with title and optional footer

---
+++ 2026/TypeScript/code/reactTs/src/components/Card.tsx
@@ -0,0 +1,19 @@
+// rfce : React Functional Component Export (Hotkey)
+
+// Importing necessary types from React
+import type { PropsWithChildren, ReactNode } from "react";
+
+interface CardProps extends PropsWithChildren {
+  title: string;
+  footer?: ReactNode;
+}
+
+export function Card({ title, children, footer }: CardProps) {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <div>{children}</div>
+      {footer && <footer>{footer}</footer>}
+    </section>
+  );
+}
