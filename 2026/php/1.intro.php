eceda7d9b2ca2155fc49ede72882741eb7939d61
Pratik Murari <pratikmurari007@gmail.com>
2026-05-14 14:35:54 +0530

    Add initial PHP introduction file with basic structure and comments

---
+++ 2026/php/1.intro.php
@@ -0,0 +1,22 @@
+<?php 
+    echo "Hello World! <br>";
+    echo "Welcome to PHP programming.";
+    // this is a single line comment
+    /*
+        This is a multi-line comment
+        You can write multiple lines of comments here
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button></button>
+</body>
+</html>
