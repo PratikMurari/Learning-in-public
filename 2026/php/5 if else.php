e9813926ead79bdfb960ab5b774645a604aab5df
Pratik Murari <pratikmurari007@gmail.com>
2026-05-21 17:22:29 +0530

    Enhance output display for age validation messages in if-else structure

--- 2026/php/5 if else.php
+++ 2026/php/5 if else.php
@@ -1,15 +1,27 @@
 <?php
-    $age = 18;
+    // if else using relational operators
+    echo "Case 1: <br>";
+    $age = -5;
     if ($age >= 100){
-        echo "You are too old to enter this site";
+        echo "You are too old to enter this site <br> <br>";
     }
     else if($age >= 18){
-        echo "You may enter this site";
+        echo "You may enter this site <br> <br>";
     }
     else if($age <= 0){
-        echo "That wasn't a valid age";
+        echo "That wasn't a valid age <br> <br>";
     }
     else {
-        echo "You must be 18+ to enter";
+        echo "You must be 18+ to enter <br> <br>";
+    }
+
+    // if else using boolean variable
+    echo "Case 2: <br>";
+    $adult = true;
+    if ($adult){
+        echo "You may enter this site <br> <br>";
+    }
+    else {
+        echo "You must be 18+ to enter <br> <br>";
     }
 ?>
