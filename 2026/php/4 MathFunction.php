89eb669a8be1c79908fadbe03630c0bb5e2f77f2
Pratik Murari <pratikmurari007@gmail.com>
2026-05-21 12:57:27 +0530

    power function

--- 2026/php/4 MathFunction.php
+++ 2026/php/4 MathFunction.php
@@ -8,7 +8,9 @@
 <body>
     <form action ="4 MathFunction.php" method="post">
         <label>x</label>
-        <input type="text" name="x">
+        <input type="text" name="x"> <br>
+        <label>y</label>
+        <input type="text" name="y"> <br>
         <input type="submit" value="solution">
 
     </form>    
@@ -17,13 +19,14 @@
 
 <?php
     $x = $_POST["x"];
+    $y = $_POST["y"];
     $solution = null;
 
     // $solution = abs($x);    
     // $solution = round($x);
     // $solution = floor($x);
-    $solution = ceil($x);
-
+    // $solution = ceil($x);
+    $solution = pow($x, $y);
 
     echo $solution;
 ?>
