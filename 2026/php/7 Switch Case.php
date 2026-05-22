b428f6adbab8069d6e9f06f0e7903a2515dc527c
Pratik Murari <pratikmurari007@gmail.com>
2026-05-22 14:59:27 +0530

    Switch case exercise

--- 2026/php/7 Switch Case.php
+++ 2026/php/7 Switch Case.php
@@ -3,24 +3,46 @@
     $grade= "D";
     switch ($grade){
         case "A":
-            echo "you did great";
+            echo "you did great <br>";
             break;
         case "B":
-            echo "you did good";
+            echo "you did good <br>";
             break;
         case "C":
-            echo "you did okay";
+            echo "you did okay <br>";
             break;
         case "D":
-            echo "you did poorly";
+            echo "you did poorly <br>";
             break;
         case "E":
-            echo "you did very poorly";
+            echo "you did very poorly <br>";
             break;
         case "F":
-            echo "you failed";
+            echo "you failed <br>";
             break;
         default: 
-            echo "invalid grade";
+            echo "invalid grade <br>";
+    }
+
+    $Day= "Monday";
+    switch ($Day){
+        case "Monday":
+            echo "I hate Monday <br>";
+            break;
+        case "Tuesday":
+            echo "Taco Tuesday <br>";
+            break;
+        case "wednesday":
+            echo "The work is half over <br>";
+            break;
+        case "Thursday":
+            echo "Stay strong soldier <br>";
+            break;
+        case "Friday":
+            echo "Weekend is almost here <br>";
+            break;
+        case "Saturday":
+            echo "Time to party <br>";
+            break;
     }
 ?>
