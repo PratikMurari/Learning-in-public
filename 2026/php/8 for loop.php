d0608eb8e96db9ba4a68ab15d861916aa2780989
Pratik Murari <pratikmurari007@gmail.com>
2026-05-23 11:09:58 +0530

    wrote some more examples for php for loop

--- 2026/php/8 for loop.php
+++ 2026/php/8 for loop.php
@@ -1,9 +1,14 @@
 <?php
     // for loop = repeat some code for a certain number amount of times
-    //
+    // 
 
     //exercise 1
-    for ($i=0; $i<7; $i++ ){
+    for ($i = 0; $i < 7; $i++ ){
         echo"Hello world! <br>";
+    }
+
+    //exercise 2
+    for ($i = 100; $i > -10; $i -= 7 ){
+        echo"$i <br>";
     }
 ?>
