c2a8ee2539fe798c6bb52d86ef4d28ab1984c3a9
Pratik Murari <pratikmurari007@gmail.com>
2026-05-26 17:23:05 +0530

    Add radio button payment selection form in PHP

---
+++ 2026/php/13 radio Button.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radio Button</title>
+</head>
+<body>
+    <form method="post">
+        <Label> Choose your mode of payment:</Label><br>
+        <input type="radio" name="payment" value="Cash"> Cash <br>
+        <input type="radio" name="payment" value="Credit Card"> Credit Card <br>
+        <input type="radio" name="payment" value="Debit Card"> Debit Card <br>
+        <input type="radio" name="payment" value="Net Banking"> Net Banking <br>
+        <input type="submit" name="confirm" value="Confirm">
+    </form>
+</body>
+</html>
+
+<?php
+    if (isset($_POST["confirm"])){
+        if (isset ($_POST["payment"])){
+            echo "You have selected: " . $_POST["payment"];
+        }
+        else {
+            echo "Please select a mode of payment.";
+        }
+    }
+?>
