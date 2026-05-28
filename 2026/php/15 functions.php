c6d05945714d7e43d81307ea452517c7925a2774
Pratik Murari <pratikmurari007@gmail.com>
2026-05-28 13:16:01 +0530

    Add example function comments and improve parameter descriptions in PHP functions

--- 2026/php/15 functions.php
+++ 2026/php/15 functions.php
@@ -6,7 +6,8 @@
     //     code to be executed;
     // }
     
-    function  happyBirthday($name, $age){              // name and age are parameters, they are variables that are used in the function
+    // Example function 1
+    function  happyBirthday($name, $age){    // name and age are parameters, they are variables that are used in the function
         echo "Happy Birthday to you <br>";
         echo "Happy Birthday to you <br>";
         echo "Happy Birthday dear {$name} <br>";
@@ -15,6 +16,13 @@
         echo "Congrats! You are {$age} now <br><br><br><br>";
     }
 
-    happyBirthday("John", 21);                      // John is an argument, it is a value that is passed to the function
+    happyBirthday("John", 21);              // John & 21 is an argument, it is a value that is passed to the function
     happyBirthday("shane", 21);
+
+    // Example function 2
+    function hypotenus(float $a,int $b){    //you can also specify the type of the parameters, it is optional but it is a good practice to do so
+        return sqrt($a ** 2 + $b ** 2);
+    }
+
+    echo hypotenus(3, 4);
 ?>
